abab96417cb865da0a917138f65ae8e2b0991d25
Jeff Benton <jeff@frogrock.net>
2019-04-11 16:27:39 -0400

    remove tests that never existed

--- spec/raider_io_cli_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe RaiderIoCli do
-  it "has a version number" do
-    expect(RaiderIoCli::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-end
